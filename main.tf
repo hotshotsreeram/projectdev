353fed136a0ba741d3b3c89661012c24ffcc66fe
root <root@Sreerams_book.localdomain>
2022-09-08 13:14:10 -0400

    5th commit with var file

--- main.tf
+++ main.tf
@@ -1,54 +1,54 @@
 provider "aws" {
-  region  = "us-east-1"
+  region = var.aws_region
 }
 
-resource "aws_vpc" "some_custom_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "dev_custom_vpc" {
+  cidr_block = var.custom_vpc
 
   tags = {
-    Name = "Some Custom VPC"
+    Name = "Dev Custom VPC"
   }
 }
 
-resource "aws_subnet" "some_public_subnet" {
-  vpc_id            = aws_vpc.some_custom_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "dev_public_subnet" {
+  vpc_id            = aws_vpc.dev_custom_vpc.id
+  cidr_block        = var.public_subnet
+  availability_zone = var.aws_zone
 
   tags = {
-    Name = "Some Public Subnet"
+    Name = "dev Public Subnet"
   }
 }
 
-resource "aws_subnet" "some_private_subnet" {
-  vpc_id            = aws_vpc.some_custom_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "dev_private_subnet" {
+  vpc_id            = aws_vpc.dev_custom_vpc.id
+  cidr_block        = var.private_subnet
+  availability_zone = var.aws_zone
 
   tags = {
-    Name = "Some Private Subnet"
+    Name = "dev Private Subnet"
   }
 }
 
-resource "aws_internet_gateway" "some_ig" {
-  vpc_id = aws_vpc.some_custom_vpc.id
+resource "aws_internet_gateway" "dev_ig" {
+  vpc_id = aws_vpc.dev_custom_vpc.id
 
   tags = {
-    Name = "Some Internet Gateway"
+    Name = "dev Internet Gateway"
   }
 }
 
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.some_custom_vpc.id
+  vpc_id = aws_vpc.dev_custom_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.some_ig.id
+    cidr_block = var.route_table
+    gateway_id = aws_internet_gateway.dev_ig.id
   }
 
   route {
     ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.some_ig.id
+    gateway_id      = aws_internet_gateway.dev_ig.id
   }
 
   tags = {
@@ -57,41 +57,41 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public_1_rt_a" {
-  subnet_id      = aws_subnet.some_public_subnet.id
+  subnet_id      = aws_subnet.dev_public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_security_group" "web_sg" {
   name   = "HTTP and SSH"
-  vpc_id = aws_vpc.some_custom_vpc.id
+  vpc_id = aws_vpc.dev_custom_vpc.id
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.route_table]
   }
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.route_table]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.route_table]
   }
 }
 
 resource "aws_instance" "web_instance" {
-  ami           = "ami-052efd3df9dad4825"
-  instance_type = "t2.micro"
-  key_name      = "My_practice"
+  ami           = var.ami
+  instance_type = var.type
+  key_name      = var.key
 
-  subnet_id                   = aws_subnet.some_public_subnet.id
+  subnet_id                   = aws_subnet.dev_public_subnet.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
@@ -111,6 +111,6 @@ resource "aws_instance" "web_instance" {
   EOF
 
   tags = {
-    "Name" : "dev"
+    "Name" = var.tags
   }
 }
