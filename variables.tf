81fc0661f4f545c45b379fdc37d7a64db3580987
root <root@Sreerams_book.localdomain>
2022-09-08 15:01:46 -0400

    5th commit with var and values file

--- variables.tf
+++ variables.tf
@@ -1,61 +1,77 @@
 variable "aws_region" {
   type        = string
   description = "aws region for ec2 insatnce "
-  default     = "us-east-1"
 }
 
 variable "aws_zone" {
   type        = string
   description = "aws zone for ec2 instance"
-  default     = "us-east-1a"
 }
 
 variable "ami" {
   type        = string
   description = "ami id for ec2 instance"
-  default     = "ami-052efd3df9dad4825"
 }
 
 variable "type" {
   type        = string
   description = "ami instance type"
-  default     = "t2.micro"
   sensitive   = true
 }
 
-variable "tags" {
+variable "vm_tags" {
   type        = string
   description = "tags for ec2 instance"
-  default     = "dev"
 }
+
+variable "vpc_tags" {
+  type        = string
+  description = "tags for vpc"
+}
+
+variable "public_subnet_tags" {
+  type        = string
+  description = "tags for public subnet"
+}
+
+variable "private_subnet_tags" {
+  type        = string
+  description = "tags for private subnet"
+}
+
+variable "internet_gateway_tags" {
+  type        = string
+  description = "tags for internet gateway"
+}
+
+variable "route_table_tags" {
+  type        = string
+  description = "tags for route table"
+}
+
 variable "key" {
   type        = string
   description = "key for ec2 instance"
-  default     = "My_practice"
   sensitive   = true
 }
 
 variable "public_subnet" {
   type        = string
   description = "public subnet ip cidr block"
-  default     = "10.0.1.0/24"
 }
 
 variable "private_subnet" {
   type        = string
   description = "private subnet ip cidr block"
-  default     = "10.0.2.0/24"
 }
 
 variable "route_table" {
   type        = string
   description = "route table ip cidr block"
-  default     = "0.0.0.0/0"
 }
 
 variable "custom_vpc" {
   type        = string
   description = "custom vpc ip cidr block"
-  default     = "10.0.0.0/16"
 }
 
